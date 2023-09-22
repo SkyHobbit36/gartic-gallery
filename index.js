1e262a6db87da5bc15f06853fb217aadfe4a73b3
SkyHobbit <skyhobbit@yandex.ru>
2023-09-22 23:30:50 +0300

    feat: сортировка для галерей

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ import files from './files.json' assert {type: 'json'};
 
 const main = document.querySelector('.main');
 
-for (const file of files) {
-    const { name, source } = file;
-    const path = 'files/' + name + '/';
-    const firsImagePath = `${path}frame-0.png`;
+files
+    .sort((a, b) => Number(a.name) - Number(b.name))
+    .forEach(file => {
+        const { name, source } = file;
+        const path = 'files/' + name + '/';
+        const firsImagePath = `${path}frame-0.png`;
 
-    const gallery = document.createElement('a');
-    gallery.className = 'gallery';
-    gallery.setAttribute('data-fancybox', 'gallery' + name);
-    gallery.setAttribute('href', firsImagePath);
+        const gallery = document.createElement('a');
+        gallery.className = 'gallery';
+        gallery.setAttribute('data-fancybox', 'gallery' + name);
+        gallery.setAttribute('href', firsImagePath);
 
-    const img = document.createElement('img');
-    img.src = firsImagePath;
-    gallery.append(img);
+        const img = document.createElement('img');
+        img.src = firsImagePath;
+        gallery.append(img);
 
-    source
-        .sort((a, b) => Number(a.replace(/frame-(\d+).png/gi, '$1')) - Number(b.replace(/frame-(\d+).png/gi, '$1')))
-        .forEach((imageSrc, index) => {
-            if (index !== 0) {
-                const a = document.createElement('a');
-                a.setAttribute('data-fancybox', 'gallery' + name);
-                a.setAttribute('href', path + '/' + imageSrc);
-                gallery.append(a);
-            }
-        })
+        source
+            .sort((a, b) => Number(a.replace(/frame-(\d+).png/gi, '$1')) - Number(b.replace(/frame-(\d+).png/gi, '$1')))
+            .forEach((imageSrc, index) => {
+                if (index !== 0) {
+                    const a = document.createElement('a');
+                    a.setAttribute('data-fancybox', 'gallery' + name);
+                    a.setAttribute('href', path + '/' + imageSrc);
+                    gallery.append(a);
+                }
+            })
 
-    main.append(gallery);
-}
+        main.append(gallery);
+    });
 
 
 Fancybox.bind("[data-fancybox]", {
